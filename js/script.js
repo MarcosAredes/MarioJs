d356b5ef9aa576d65752da23c663854810fbd0eb
Marcos <106114852+MarcosAredes@users.noreply.github.com>
2024-01-27 22:46:55 -0300

    A porra do SCORE

--- js/script.js
+++ js/script.js
@@ -1,41 +1,46 @@
 const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
 const reloadButton = document.getElementById('reloadButton');
+const scoreDisplay = document.getElementById('scoreDisplay');
 let loop;
-const jump = () =>{
+let score = 0;
+
+const jump = () => {
     mario.classList.add('jump');
 
-    setTimeout(()=>{
+    setTimeout(() => {
         mario.classList.remove('jump');
-        
-    },500);
+    }, 500);
 }
 
-    loop = setInterval(()=>{
-    
+loop = setInterval(() => {
     const pipePosition = pipe.offsetLeft;
-    const marioPosition = + window.getComputedStyle(mario).bottom.replace('px','');
-    
+    const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
 
     if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 108) {
         pipe.style.animation = 'none';
         pipe.style.left = `${pipePosition}px`;
-        
+
         mario.style.animation = 'none';
         mario.style.bottom = `${marioPosition}px`;
 
-        mario.src ='./img/game-over.png';
+        mario.src = './img/game-over.png';
         mario.style.width = '75px'
         mario.style.marginLeft = '50px'
-        
+
         clearInterval(loop);
 
         reloadButton.style.display = 'block';
+
+        console.log('Score:', score);
+    } else {
+        score++;
+        scoreDisplay.textContent = `Score: ${score}`;
     }
+}, 10);
 
-},10)  
+document.addEventListener('keydown', jump);
 
-document.addEventListener('keydown',jump)
 reloadButton.addEventListener('click', () => {
     location.reload();
 });
